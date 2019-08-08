e0fc3a65965cacd399257b971003557d7b2a5923
sunchao199606 <17854230810@163.com>
2019-08-08 21:26:16 +0800

    Vue实例中data属性的三种写法

--- src/vue/login.vue
+++ src/vue/login.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    <h2>login.vue</h2>
+    <h2>{{message}}</h2>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  // ES6
+  data() {
+    return {
+      message: "login component"
+    };
+  },
+  //ES6 arrow function
+  data: () => {
+    return {
+      message: "login component"
+    };
+  },
+  // ES5
+  data: function() {
+    return {
+      message: "login component"
+    };
+  }
+};
 </script>
 
 <style>
