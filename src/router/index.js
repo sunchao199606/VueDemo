48d717889b3553f15d7bd3970f59fa75b1651a04
sunchao199606 <17854230810@163.com>
2020-08-26 14:32:42 +0800

    路由重定向

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,16 @@ import Vue from 'vue'
 import login from '&/components/login'
 import logout from '&/components/logout'
 import stock from '&/components/stock'
+
 Vue.use(Router);
 
 const routes = [
-    { path: '/login', component: login, name: 'login' },
-    { path: '/logout', component: logout, name: 'logout' },
-    { path: '/stock', component: stock, name: 'stock' }
+    {path: '/login', component: login, name: 'login'},
+    {path: '/logout', component: logout, name: 'logout'},
+    {path: '/stock', component: stock, name: 'stock'},
+    {
+        path: '/a', component: stock, name: 'testRedirect', redirect: '/b'
+    }
 ];
 
-export const router = new Router({ routes });
+export const router = new Router({routes});
