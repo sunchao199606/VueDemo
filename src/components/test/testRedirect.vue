f27f070acb9ce21b3bd24454fd2824de3ec77f4d
sunchao199606 <17854230810@163.com>
2020-09-18 16:07:34 +0800

    重定向测试

---
+++ src/components/test/testRedirect.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Redirect</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "testRedirect",
+        data() {
+            console.log("aaaaaa");
+            console.log(this.$route);
+            console.log(this.$router);
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
