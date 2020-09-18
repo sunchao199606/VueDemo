1fb21d34d43a884230c20066276283fcbb969532
sunchao199606 <17854230810@163.com>
2020-09-18 16:06:05 +0800

    侧边栏测试

---
+++ src/components/sidebar/SideBarItems.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-for="(item, aa) in items" :key="aa">
+            <el-submenu index="item.path" v-if="hasChildren(item)">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span slot="title">{{item.meta.title}}</span>
+                </template>
+                <side-bar-items :items="item.children"></side-bar-items>
+            </el-submenu>
+            <el-menu-item index="item.path" v-else>{{item.meta.title}}</el-menu-item>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SideBarItems",
+        props: ["items"],
+        methods: {
+            hasChildren(route) {
+                return route.children && route.children.length > 0
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/sidebar/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-aside>
+        <el-menu>
+            <side-bar-items :items="routes"></side-bar-items>
+        </el-menu>
+    </el-aside>
+</template>
+
+<script>
+    import SideBarItems from "&/components/sidebar/SideBarItems"
+
+    export default {
+        name: "SideBar",
+        components: {SideBarItems},
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
