a674376dca6227ed5d92615649800be896f27974
sunchao199606 <17854230810@163.com>
2020-11-02 10:08:33 +0800

    slot测试

---
+++ src/components/test/SlotDemo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <el-container>
+            <el-header>
+                <slot name="A"></slot>
+            </el-header>
+            <el-aside></el-aside>
+        </el-container>
+
+        <slot name="B"></slot>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SlotDemo"
+    }
+</script>
+
+<style scoped>
+
+</style>
